37a7cb84627855faf207d7839c88bdda7c8a2d9f
shine <AuroreDeveloper@gmail.com>
2017-06-21 14:37:20 +0900

    view 만들다가 하루 다가겠네.

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Link(models.Model):
     url = models.URLField(unique=True)
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
 
 def main_page(request):
-    output = '''
-    <html>
-        <head><title>%s</title></head>
-            <body>
-                <h1>%s</h1><p>%s</p>
-            </body>
-        </html>
-                
-    '''% (
-        '장고 | 북마크',
-        '장고 북마크에 환영합니다.',
-        '여기에 북마크를 저장하고 공유합니다.'
-    )
-    return HttpResponse(output)
+  template = get_template('main_page.html')
+  variables = Context({
+    'head_title': 'Django Bookmarks',
+    'page_title': 'Welcome to Django Bookmarks',
+    'page_body': 'Where you can store and share bookmarks!'
+  })
+  output = template.render(variables)
+  return HttpResponse(output)
